a9b94a1b1cdb2ec0bba5e1872b72a457afaa8120
shanielia <selia781@student.fuhsd.org>
2019-10-21 12:22:17 -0700

    added word object

--- src/Word.java
+++ src/Word.java
@@ -1,3 +1,21 @@
 public class Word {
-    
+    String word;
+    long freq;
+    public Word(String word, long frequency){
+        this.freq = frequency;
+        this.word = word;
+    }
+    public String getWord(){
+        return word;
+    }
+    public long getFreq(){
+        return freq;
+    }
+    public void setWord( String nWord){
+        this.word = nWord;
+    }
+    public void setFreq(long nFreq){
+        this.freq = nFreq;
+    }
+
 }
